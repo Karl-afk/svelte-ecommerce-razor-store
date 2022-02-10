15275050bf4d605a02abf1767eb6d7b7b7f9d45d
Karl Weinmueller <karlweinmueller@me.com>
2022-02-10 07:21:09 +0100

    frontent finished

---
+++ src/components/LoginLink.svelte
@@ -0,0 +1,24 @@
+<script>
+  import user from "../stores/user";
+  import { link } from "svelte-routing";
+  // logout function
+  import globalStore from "../stores/globalStore";
+</script>
+
+{#if $user.jwt}
+  <!-- content here -->
+  <a
+    href="/"
+    class="logout-btn"
+    on:click={() => {
+      globalStore.toggleItem("sidebar", false);
+    }}>logout</a
+  >
+{:else}
+  <a
+    href="/login"
+    on:click={() => {
+      globalStore.toggleItem("sidebar", false);
+    }}>login</a
+  >
+{/if}
--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,18 +1,28 @@
 <script>
-  import { onMount, onDestroy } from "svelte";
+  export let title = "";
   import products from "../../stores/defaultProducts";
+  import Product from "./Product.svelte";
+  import Loading from "../Loading.svelte";
+  //   let unsubscribe;
 
-  let localProducts = [];
-  onMount(() => {
-    products.subscribe((value) => {
-      localProducts = value;
-    });
-  });
+  //   onMount(() => {
+  //     unsubscribe = products.subscribe((value) => {
+  //       console.log("subscribed");
+  //       localProducts = value;
+  //     });
+  //   });
+  //   onDestroy(() => {
+  //     unsubscribe();
+  //   });
 </script>
 
-<h1>Products Component</h1>
-
-{#each localProducts as product}
-  <!-- content here -->
-  <h2>{product.title}</h2>
-{/each}
+<section class="section">
+  <h2 class="section-title">{title}</h2>
+  <div class="products-center">
+    {#each $products as product (product.id)}
+      <Product {product} />
+    {:else}
+      <Loading />
+    {/each}
+  </div>
+</section>
